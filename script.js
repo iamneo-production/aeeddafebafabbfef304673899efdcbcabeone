3f2b896fe062afad0c52954f693c00ea5785f105
Iamneo <techadmin@iamneo.ai>
2023-09-03 11:01:14 +0000

    Backup Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let btnRef=document.querySelector(".btn");
+let popRef=document.querySelector(".popup");
+let newgameBtn=document.getElementById("new-game");
+let resetBtn=document.getElementById("reset");
+let msfRef=document.getElementById("message");
+let winningPattern=[
+    [0,1,2],
+    [0,3,6],
+    [2,5,8],
+    [6,7,8],
+    [3,4,5],
+    [1,4,7],
+    [0,4,8],
+    [2,4,6]
+]
+let xTurn=true;
+let count =0;
